968f6900f43c9cee6e8ff23487bb6b20871cba50
parksaneon <sangeon1500@daum.net>
2022-02-08 15:40:02 +0900

    faet : block when short length state by useRef

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,6 +1,9 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 const DiaryEditor = () => {
+  const authorInput = useRef();
+  const contentInput = useRef();
+
   const [state, setState] = useState({
     author: '',
     content: '',
@@ -13,12 +16,12 @@ const DiaryEditor = () => {
 
   const handleSubmit = () => {
     if (state.author.length < 1) {
-      alert('작성자는 최소 한글자 이상 입력해주세요');
+      authorInput.current.focus();
       return;
     }
 
     if (state.content.length < 5) {
-      alert('일기 본문은 최소 다섯글자 이상 입력해주세요');
+      contentInput.current.focus();
       return;
     }
     console.log(state);
@@ -28,10 +31,10 @@ const DiaryEditor = () => {
     <div className='DiaryEditor'>
       <h2>오늘의 일기</h2>
       <div>
-        <input name='author' value={state.author} onChange={handleChangeState} />
+        <input ref={authorInput} name='author' value={state.author} onChange={handleChangeState} />
       </div>
       <div>
-        <textarea name='content' value={state.content} onChange={handleChangeState} />
+        <textarea ref={contentInput} name='content' value={state.content} onChange={handleChangeState} />
       </div>
       <div>
         <select name='emotion' value={state.emotion} onChange={handleChangeState}>
