a636cabe8e8a61cc7991f62b637f281aa9f03a77
parksaneon <sangeon1500@daum.net>
2022-02-08 09:37:35 +0900

    two useState for input, textarea

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -2,12 +2,21 @@ import { useState } from 'react';
 
 const DiaryEditor = () => {
   const [author, setAuthor] = useState('');
+  const [content, setContent] = useState('');
 
   return (
     <div className="DiaryEditor">
       <h2>오늘의 일기</h2>
       <div>
-        <input type="text" />
+        <input
+          name="author"
+          type="text"
+          value={author}
+          onChange={({ target: { value } }) => setAuthor(value)}
+        />
+      </div>
+      <div>
+        <textarea value={content} onChange={({ target: { value } }) => setContent(value)} />
       </div>
     </div>
   );
