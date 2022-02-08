3a5e5a407dfa61e011753cbd97b6031d9b859233
parksaneon <sangeon1500@daum.net>
2022-02-08 16:01:23 +0900

    faet : DiaryItem component

--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,3 +1,5 @@
+import DiaryItem from './DiaryItem';
+
 const DiaryList = ({ diaryList }) => {
   return (
     <div className='DiaryList'>
@@ -5,16 +7,15 @@ const DiaryList = ({ diaryList }) => {
       <h4>{diaryList.length} 개의 일기가 있습니다.</h4>
       <ul>
         {diaryList.map(diary => (
-          <li>
-            <div>작성자 : {diary.author}</div>
-            <div>일기 : {diary.content}</div>
-            <div>감정 : {diary.emotion}</div>
-            <div>작성 시간(ms) : {diary.created_date}</div>
-          </li>
+          <DiaryItem key={diary.id} {...diary} />
         ))}
       </ul>
     </div>
   );
+};
+
+DiaryList.defaultProps = {
+  diaryList: []
 };
 
 export default DiaryList;
