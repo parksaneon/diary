1ec264bd957028ac61c3b158d2bc62cca0e662af
parksaneon <sangeon1500@daum.net>
2022-02-08 16:55:41 +0900

    faet : edit DiaryItem content

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
     setData([newItem, ...data]);
   };
 
-  const onDelete = targetId => {
+  const onRemove = targetId => {
     console.log(`${targetId}가 삭제됐습니다.`);
     const newDiaryList = data.filter(diary => diary.id !== targetId);
     setData(newDiaryList);
@@ -52,7 +52,7 @@ function App() {
   return (
     <div className='App'>
       <DiaryEditor onCreate={onCreate} />
-      <DiaryList diaryList={data} onDelete={onDelete} />
+      <DiaryList diaryList={data} onRemove={onRemove} />
     </div>
   );
 }
--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,4 +1,16 @@
-const DiaryItem = ({ id, author, content, emotion, created_date, onDelete }) => {
+import { useState } from 'react';
+
+const DiaryItem = ({ id, author, content, emotion, created_date, onRemove }) => {
+  const [isEdit, setIsEdit] = useState(false);
+
+  const handleRemove = () => {
+    if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
+      onRemove(id);
+    }
+  };
+
+  const toggleIsEdit = () => setIsEdit(!isEdit);
+
   return (
     <li className='DiaryItem'>
       <div className='info'>
@@ -9,13 +21,8 @@ const DiaryItem = ({ id, author, content, emotion, created_date, onDelete }) => 
         <span className='date'>{new Date(created_date).toLocaleString()}</span>
       </div>
       <div className='content'>{content}</div>
-      <button
-        onClick={() => {
-          if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) onDelete(id);
-        }}
-      >
-        삭제하기
-      </button>
+      <button onClick={handleRemove}>삭제하기</button>
+      <button onClick={toggleIsEdit}>수정하기</button>
     </li>
   );
 };
