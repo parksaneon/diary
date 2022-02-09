b3607915c43469e71641ee5451410458ed3959c2
parksaneon <sangeon1500@daum.net>
2022-02-09 12:48:02 +0900

    feat : add optimize React.memo exaple

--- src/OptimizeTest.js
+++ src/OptimizeTest.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
+// re-rendering
 const CountView = React.memo(({ count }) => {
   useEffect(() => {
     console.log(`Update count : ${count}`);
@@ -7,6 +8,7 @@ const CountView = React.memo(({ count }) => {
   return <div>{count}</div>;
 });
 
+// re-rendering
 const TextView = React.memo(({ text }) => {
   useEffect(() => {
     console.log(`Update text : ${text}`);
@@ -14,9 +16,27 @@ const TextView = React.memo(({ text }) => {
   return <div>{text}</div>;
 });
 
+// no re-rendering
+const Count2View = React.memo(({ count2 }) => {
+  useEffect(() => {
+    console.log(`Update count2 : ${count2}`);
+  });
+  return <div>{count2}</div>;
+});
+
+// re-rendering
+const CountObjView = React.memo(({ obj }) => {
+  useEffect(() => {
+    console.log(`Update obj : ${obj.count}`);
+  });
+  return <div>{obj.count}</div>;
+});
+
 const OptimizeTest = () => {
   const [count, setCount] = useState(1);
+  const [count2, setCount2] = useState(1);
   const [text, setText] = useState('');
+  const [obj, setObj] = useState({ count: 1 });
 
   return (
     <div>
@@ -26,9 +46,25 @@ const OptimizeTest = () => {
         <button onClick={() => setCount(count + 1)}>+</button>
       </div>
       <div>
+        <h2>count 2</h2>
+        <Count2View count={count2} />
+        <button onClick={() => setCount2(count + 1)}>+</button>
+      </div>
+      <div>
         <h2>text</h2>
         <TextView text={text} />
         <input value={text} onClick={() => setText(e.target.value)} />
+      </div>
+      <div>
+        <h2>count Obj</h2>
+        <CountObjView obj={obj} />
+        <button
+          onClick={() => {
+            setObj({ count: obj.count });
+          }}
+        >
+          Obj btn
+        </button>
       </div>
     </div>
   );
